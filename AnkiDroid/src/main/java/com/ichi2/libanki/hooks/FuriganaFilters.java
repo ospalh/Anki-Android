6b328c3dd16fcea0a2120d1cc06ce1d33c036d31
ospalh <ospalh@gmail.com>
2015-05-19 10:44:08 +0200

    Recode of the furikanji code. My special word character based base text matcher still does not work.

--- AnkiDroid/src/main/java/com/ichi2/libanki/hooks/FuriganaFilters.java
+++ AnkiDroid/src/main/java/com/ichi2/libanki/hooks/FuriganaFilters.java
@@ -22,19 +22,31 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FuriganaFilters {
-    private static final Pattern r = Pattern.compile(" ?([^ >]+?)\\[(.+?)\\]");
+    private static final Pattern READING = Pattern.compile(" ?([^ >]+?)\\[(.+?)\\]");
+    // private static final Pattern READING = Pattern.compile(" ?([-+×÷%\\.\\w]+?)\\[(.+?)\\]",
+    //     Pattern.UNICODE_CHARACTER_CLASS);
+    // private static final Pattern READING = Pattern.compile("(?U) ?([-+×÷%\\.\\w]+?)\\[(.+?)\\]");
+
+    // Bah. Using Pattern.UNICODE_CHARACTER_CLASS doesn’t compile. No wonder that using the “(?U)” tag does not
+    // work. Using the “\\w” pattern without a Unicode-indicator seems to work with Japanese text, but beter not
+    // risk anything and use the more standard matching and add code point 0x20 spaces.
 
     // Since there is no ruby tag support in Android before 3.0 (SDK version 11), we must use an alternative
     // approach to align the elements. Anki does the same thing in aqt/qt.py for earlier versions of qt.
     // The fallback approach relies on CSS in the file /assets/ruby.css
-    private static final String RUBY = CompatHelper.isHoneycomb() ? "<ruby><rb>$1</rb><rt>$2</rt></ruby>"
-            : "<span class='legacy_ruby_rb'><span class='legacy_ruby_rt'>$2</span>$1</span>";
+    private static final String FURIGANA = CompatHelper.isHoneycomb() ?
+        "<ruby class='furigana'><rb>$1</rb><rt>$2</rt></ruby>" :
+        "<span class='furigana legacy_ruby_rb'><span class='legacy_ruby_rt'>$2</span>$1</span>";
+    private static final String FURIKANJI = CompatHelper.isHoneycomb() ?
+        "<ruby class='furikanji'><rb>$2</rb><rt>$1</rt></ruby>" :
+        "<span class='furikanji legacy_ruby_rb'><span class='legacy_ruby_rt'>$1</span>$2</span>";
 
 
     public void install(Hooks h) {
         h.addHook("fmod_kanji", new Kanji());
         h.addHook("fmod_kana", new Kana());
         h.addHook("fmod_furigana", new Furigana());
+        h.addHook("fmod_furikanji", new Furikanji());
     }
 
 
@@ -43,14 +55,14 @@ public class FuriganaFilters {
             // return without modification
             return match.group(0);
         } else {
-            return r.matcher(match.group(0)).replaceAll(repl);
+            return READING.matcher(match.group(0)).replaceAll(repl);
         }
     }
 
     public class Kanji extends Hook {
         @Override
         public Object runFilter(Object arg, Object... args) {
-            Matcher m = r.matcher((String) arg);
+            Matcher m = READING.matcher((String) arg);
             StringBuffer sb = new StringBuffer();
             while (m.find()) {
                 m.appendReplacement(sb, noSound(m, "$1"));
@@ -63,7 +75,7 @@ public class FuriganaFilters {
     public class Kana extends Hook {
         @Override
         public Object runFilter(Object arg, Object... args) {
-            Matcher m = r.matcher((String) arg);
+            Matcher m = READING.matcher((String) arg);
             StringBuffer sb = new StringBuffer();
             while (m.find()) {
                 m.appendReplacement(sb, noSound(m, "$2"));
@@ -76,10 +88,23 @@ public class FuriganaFilters {
     public class Furigana extends Hook {
         @Override
         public Object runFilter(Object arg, Object... args) {
-            Matcher m = r.matcher((String) arg);
+            Matcher m = READING.matcher((String) arg);
             StringBuffer sb = new StringBuffer();
             while (m.find()) {
-                m.appendReplacement(sb, noSound(m, RUBY));
+                m.appendReplacement(sb, noSound(m, FURIGANA));
+            }
+            m.appendTail(sb);
+            return sb.toString();
+        }
+    }
+
+    public class Furikanji extends Hook {
+        @Override
+        public Object runFilter(Object arg, Object... args) {
+            Matcher m = READING.matcher((String) arg);
+            StringBuffer sb = new StringBuffer();
+            while (m.find()) {
+                m.appendReplacement(sb, noSound(m, FURIKANJI));
             }
             m.appendTail(sb);
             return sb.toString();
